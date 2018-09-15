bd7b3ec51e7f0954f95eb4e364d7d454e725b392
Pawan Kumar <jmppawanhit@gmail.com>
2018-09-15 20:33:26 +0530

    Basic usb driver skleton

--- Makefile
+++ Makefile
@@ -1,1 +1,13 @@
+TARGET:= usbdriver
+PWD:= $(shell pwd)
+EXTRA_CFLAGS:=-Wno-error
+KERNDIR:= "/lib/modules/$(shell uname -r)/build/"
+CC:=gcc
 
+obj-m:= ${TARGET}.o
+
+all:
+	make -C ${KERNDIR} M=${PWD}  modules
+
+clean:
+	make -C ${KERNDIR} M=${PWD}  clean
--- usbdriver.h
+++ usbdriver.h
@@ -1,1 +1,22 @@
+/* @Module: user-test-driver.c  
+ * @Author: Pawan Kumar
+ * @Email: jmppawanhit@gmail.com
+ * @License: GPL
+ * @Domain: Kernel Module Programming
+ * @Description: 
+ * @Copyright: Copyright (C) 2018 pkmys@github.com
+ */
 
+#ifndef _USBDRIVER_h
+#define _USBDRIVER_h
+#endif
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#define DEVICE_NAME "usbdev" 
+#define MOD_AUTHOR "Pawan Kumar <jmppawanhit@gmail.com>"
+#define MOD_DESC "go_usb driver"
+#define MOD_SUPPORT "usb device"
+#define USB_VENDOR_ID 0x0781
+#define USB_PRODUCT_ID 0x558a
