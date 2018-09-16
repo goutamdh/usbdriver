40090267cdf7b2b114844160bee80c38904fd3ff
Pawan Kumar <jmppawanhit@gmail.com>
2018-09-16 16:23:40 +0530

    cosmetic change

--- go_driver.h
+++ go_driver.h
@@ -6,25 +6,19 @@
  * @Description: 
  * @Copyright: Copyright (C) 2018 pkmys@github.com
  */
+
+#ifndef _GO_DRIVER_H_
+#define _GO_DRIVER_H_
+#endif
+
 /******************************************
  *               includes                 *
  ******************************************/
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/device.h>
 #include <linux/init.h>
 #include <linux/usb.h>
 #include <linux/slab.h>
-
-#ifndef _GO_DRIVER_H_
-#define _GO_DRIVER_H_
-#endif
-#define DEBUG
-#define DEBUG_LEVEL_DEBUG 0x1F
-#define DEBUG_LEVEL_INFO 0x0F
-#define DEBUG_LEVEL_WARN 0x07
-#define DEBUG_LEVEL_ERROR 0x03
-#define DEBUG_LEVEL_CRITICAL 0x01
 
 #define DBG_DEBUG(fmt, args...)                                 \
     if ((debug_level & DEBUG_LEVEL_DEBUG) == DEBUG_LEVEL_DEBUG) \
@@ -47,10 +41,17 @@
     printk(KERN_CRIT "[crit]  %s(%d): " fmt "\n",                     \
            __FUNCTION__, __LINE__, ##args)
 
-#define USB_VENDOR_ID 0x0781
-#define USB_PRODUCT_ID 0x558a
+#define DEBUG
+#define DEBUG_LEVEL_DEBUG     0x1F
+#define DEBUG_LEVEL_INFO      0x0F
+#define DEBUG_LEVEL_WARN      0x07
+#define DEBUG_LEVEL_ERROR     0x03
+#define DEBUG_LEVEL_CRITICAL  0x01
 
-#define DEVICE_NAME "usbdev"
+#define USB_VENDOR_ID         0x0781
+#define USB_PRODUCT_ID        0x558a
+
+#define DEVICE_NAME "go_usb"
 #define MOD_AUTHOR "Pawan Kumar <jmppawanhit@gmail.com>"
 #define MOD_DESC "go_usb driver"
 #define MOD_SUPPORT "usb device"
